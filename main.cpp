b72265b33887a858d1a9ff6cd23169eaee4ac7f3
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-13 19:10:10 -0300

    commit 1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+
+    int idade;
+
+    std::cout<<"Digite a sua idade: "<<std::endl;
+    std::cin>>idade;
+
+    if(idade >= 18){
+        std::cout<<"Você é maior de idade!"<<std::endl;
+    }else{
+        std::cout<<"Você é menor de idade!!"<<std::endl;
+    }
+
+
+return 0;
+}
