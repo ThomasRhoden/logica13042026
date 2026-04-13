436b18966950fa46dfd408764481efc179955677
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-13 19:42:55 -0300

    commit 2

--- main.cpp
+++ main.cpp
@@ -7,8 +7,18 @@ int main(){
     std::cout<<"Digite a sua idade: "<<std::endl;
     std::cin>>idade;
 
-    if(idade <= 0){
+    if(idade < 0){
         std::cout<<"Idade inválida!"<<std::endl;
+    }else if(idade <= 2){
+        std::cout<<"Você é um bebê!"<<std::endl;
+    }else if(idade <= 8){
+        std::cout<<"Você é uma criança!"<<std::endl;
+    }else if(idade <= 17){
+        std::cout<<"Você é um adolescente!"<<std::endl;
+    }else{
+        std::cout<<"Você é um adulto!"<<std::endl;
     }
+
+
 return 0;
 }
